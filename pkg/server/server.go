60a029690df85abc6321cbc687dd3900852dd382
Gabriel Malinosqui <gabrielmalinosqui@gmail.com>
2026-05-27 11:02:51 -0300

    Initialize feature/concurrent-datasource-queries

---
+++ pkg/server/server.go
@@ -0,0 +1,185 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+
+	"github.com/grafana/grafana/pkg/api"
+	_ "github.com/grafana/grafana/pkg/extensions"
+	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/infra/metrics"
+	"github.com/grafana/grafana/pkg/infra/usagestats/statscollector"
+	"github.com/grafana/grafana/pkg/modules"
+	moduleRegistry "github.com/grafana/grafana/pkg/modules/registry"
+	"github.com/grafana/grafana/pkg/registry"
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/services/provisioning"
+	"github.com/grafana/grafana/pkg/setting"
+)
+
+// Options contains parameters for the New function.
+type Options struct {
+	HomePath    string
+	PidFile     string
+	Version     string
+	Commit      string
+	BuildBranch string
+	Listener    net.Listener
+}
+
+// New returns a new instance of Server.
+func New(opts Options, cfg *setting.Cfg, httpServer *api.HTTPServer, roleRegistry accesscontrol.RoleRegistry,
+	provisioningService provisioning.ProvisioningService,
+	usageStatsProvidersRegistry registry.UsageStatsProvidersRegistry, statsCollectorService *statscollector.Service,
+	moduleService modules.Engine,
+	_ moduleRegistry.Registry, // imported to invoke initialization via Wire
+) (*Server, error) {
+	statsCollectorService.RegisterProviders(usageStatsProvidersRegistry.GetServices())
+	s, err := newServer(opts, cfg, httpServer, roleRegistry, provisioningService, moduleService)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = s.init(context.Background()); err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+func newServer(opts Options, cfg *setting.Cfg, httpServer *api.HTTPServer, roleRegistry accesscontrol.RoleRegistry,
+	provisioningService provisioning.ProvisioningService,
+	moduleService modules.Engine) (*Server, error) {
+	return &Server{
+		HTTPServer:          httpServer,
+		provisioningService: provisioningService,
+		roleRegistry:        roleRegistry,
+		shutdownFinished:    make(chan struct{}),
+		log:                 log.New("server"),
+		cfg:                 cfg,
+		pidFile:             opts.PidFile,
+		version:             opts.Version,
+		commit:              opts.Commit,
+		buildBranch:         opts.BuildBranch,
+		moduleService:       moduleService,
+	}, nil
+}
+
+// Server is responsible for managing the lifecycle of services.
+type Server struct {
+	log              log.Logger
+	cfg              *setting.Cfg
+	shutdownOnce     sync.Once
+	shutdownFinished chan struct{}
+	isInitialized    bool
+	mtx              sync.Mutex
+
+	pidFile     string
+	version     string
+	commit      string
+	buildBranch string
+
+	HTTPServer          *api.HTTPServer
+	roleRegistry        accesscontrol.RoleRegistry
+	provisioningService provisioning.ProvisioningService
+	moduleService       modules.Engine
+}
+
+// init initializes the server and its services.
+func (s *Server) init(ctx context.Context) error {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	if s.isInitialized {
+		return nil
+	}
+	s.isInitialized = true
+
+	if err := s.writePIDFile(); err != nil {
+		return err
+	}
+
+	// Initialize dskit modules.
+	if err := s.moduleService.Init(ctx); err != nil {
+		return err
+	}
+
+	if err := metrics.SetEnvironmentInformation(s.cfg.MetricsGrafanaEnvironmentInfo); err != nil {
+		return err
+	}
+
+	if err := s.roleRegistry.RegisterFixedRoles(ctx); err != nil {
+		return err
+	}
+
+	return s.provisioningService.RunInitProvisioners(ctx)
+}
+
+// Run initializes and starts services. This will block until all services have
+// exited. To initiate shutdown, call the Shutdown method in another goroutine.
+func (s *Server) Run(ctx context.Context) error {
+	defer close(s.shutdownFinished)
+
+	if err := s.init(ctx); err != nil {
+		return err
+	}
+
+	err := s.moduleService.Run(ctx)
+	if err != nil && !errors.Is(err, context.Canceled) {
+		return err
+	}
+	return nil
+}
+
+// Shutdown initiates Grafana graceful shutdown. This shuts down all
+// running background services. Since Run blocks Shutdown supposed to
+// be run from a separate goroutine.
+func (s *Server) Shutdown(ctx context.Context, reason string) error {
+	var err error
+	s.shutdownOnce.Do(func() {
+		s.log.Info("Shutdown started", "reason", reason)
+		if err = s.moduleService.Shutdown(ctx); err != nil {
+			s.log.Error("Failed to shutdown modules", "error", err)
+		}
+		// Wait for server to shut down
+		select {
+		case <-s.shutdownFinished:
+			s.log.Debug("Finished waiting for server to shut down")
+		case <-ctx.Done():
+			s.log.Warn("Timed out while waiting for server to shut down")
+			err = fmt.Errorf("timeout waiting for shutdown")
+		}
+	})
+
+	return err
+}
+
+// writePIDFile retrieves the current process ID and writes it to file.
+func (s *Server) writePIDFile() error {
+	if s.pidFile == "" {
+		return nil
+	}
+
+	// Ensure the required directory structure exists.
+	err := os.MkdirAll(filepath.Dir(s.pidFile), 0700)
+	if err != nil {
+		s.log.Error("Failed to verify pid directory", "error", err)
+		return fmt.Errorf("failed to verify pid directory: %s", err)
+	}
+
+	// Retrieve the PID and write it to file.
+	pid := strconv.Itoa(os.Getpid())
+	if err := os.WriteFile(s.pidFile, []byte(pid), 0644); err != nil {
+		s.log.Error("Failed to write pidfile", "error", err)
+		return fmt.Errorf("failed to write pidfile: %s", err)
+	}
+
+	s.log.Info("Writing PID file", "path", s.pidFile, "pid", pid)
+	return nil
+}
---
+++ public/app/plugins/datasource/loki/configuration/ConfigEditor.tsx
@@ -0,0 +1,80 @@
+import React, { useCallback } from 'react';
+
+import { DataSourcePluginOptionsEditorProps, DataSourceSettings } from '@grafana/data';
+import { ConfigSection } from '@grafana/experimental';
+import { config, reportInteraction } from '@grafana/runtime';
+import { DataSourceHttpSettings } from '@grafana/ui';
+import { Divider } from 'app/core/components/Divider';
+
+import { LokiOptions } from '../types';
+
+import { AlertingSettings } from './AlertingSettings';
+import { DerivedFields } from './DerivedFields';
+import { QuerySettings } from './QuerySettings';
+
+export type Props = DataSourcePluginOptionsEditorProps<LokiOptions>;
+
+const makeJsonUpdater =
+  <T extends any>(field: keyof LokiOptions) =>
+  (options: DataSourceSettings<LokiOptions>, value: T): DataSourceSettings<LokiOptions> => {
+    return {
+      ...options,
+      jsonData: {
+        ...options.jsonData,
+        [field]: value,
+      },
+    };
+  };
+
+const setMaxLines = makeJsonUpdater('maxLines');
+const setPredefinedOperations = makeJsonUpdater('predefinedOperations');
+const setDerivedFields = makeJsonUpdater('derivedFields');
+
+export const ConfigEditor = (props: Props) => {
+  const { options, onOptionsChange } = props;
+
+  const updatePredefinedOperations = useCallback(
+    (value: string) => {
+      reportInteraction('grafana_loki_predefined_operations_changed', { value });
+      onOptionsChange(setPredefinedOperations(options, value));
+    },
+    [options, onOptionsChange]
+  );
+
+  return (
+    <>
+      <Divider />
+
+      <DataSourceHttpSettings
+        defaultUrl={'http://localhost:3100'}
+        dataSourceConfig={options}
+        showAccessOptions={false}
+        onChange={onOptionsChange}
+        secureSocksDSProxyEnabled={config.secureSocksDSProxyEnabled}
+      />
+
+      <Divider />
+
+      <ConfigSection
+        title="Additional settings"
+        description="Additional settings are optional settings that can be configured for more control over your data source."
+        isCollapsible={true}
+        isInitiallyOpen
+      >
+        <AlertingSettings options={options} onOptionsChange={onOptionsChange} />
+        <Divider hideLine />
+        <QuerySettings
+          maxLines={options.jsonData.maxLines || ''}
+          onMaxLinedChange={(value) => onOptionsChange(setMaxLines(options, value))}
+          predefinedOperations={options.jsonData.predefinedOperations || ''}
+          onPredefinedOperationsChange={updatePredefinedOperations}
+        />
+        <Divider hideLine />
+        <DerivedFields
+          fields={options.jsonData.derivedFields}
+          onChange={(value) => onOptionsChange(setDerivedFields(options, value))}
+        />
+      </ConfigSection>
+    </>
+  );
+};
